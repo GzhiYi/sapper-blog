335455d0d5a0c60cf342ae2068444bbbcf1ae0c4
GzhiYi <gzy.guy@gmail.com>
2020-06-28 22:34:28 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,11 @@ const tailwindcss = require("tailwindcss");
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./src/**/*.html", "./src/**/**/*.svelte"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  ,
+  whitelistPatternsChildren: [
+    /v/
+  ]
 });
 module.exports = {
   plugins: [
