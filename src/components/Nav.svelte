64bebe30a30a6fc331384abd0a8b24356b7de02f
GzhiYi <gongzhiyi@youmi.net>
2020-07-01 14:06:05 +0800

    feat: 修改页面路由次序。

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-	export let segment;
+	// export let isBlog = window.location.pathname === '/';
 </script>
 
 <style>
@@ -55,6 +55,6 @@
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 			the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+		<li><a rel=prefetch aria-current="blog" href="/">blog</a></li>
 	</ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,39 @@
-<script lang="typescript">
-	const title: string = "welcome"
+<script context="module">
+	export function preload({ params, query }) {
+
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
 </script>
+
+<script>
+	export let posts;
+</script>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+	a {
+		margin: 6px 0;
+	}
+</style>
+
 <svelte:head>
 	<title>GzhiYi's blog</title>
 	<meta name="description" content="web前端工程师，编程是一份兴趣，热爱着生活。">
 	<meta name="keywords" content="frontend开发者，熟悉web前端，vue，React，svelte，小程序等等。">
 </svelte:head>
-<h1>Great to see you.</h1>
+
+<ul>
+	{#each posts as post}
+		<!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+		<li><a class="e-underline" rel='prefetch' href='blog/{post.slug}'>{new Date(post.fmData.attributes.date).toLocaleDateString()} {post.fmData.attributes.title}   </a></li>
+	{/each}
+
+</ul>
